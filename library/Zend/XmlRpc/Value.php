d1a0bff261ed6b57c6c9a9e568d06e58c9372734
Alexandr Bashurov <abashurov@plesk.com>
2021-02-18 20:52:25 +0700

    TECH PHP 8.0 support

--- library/Zend/XmlRpc/Value.php
+++ library/Zend/XmlRpc/Value.php
@@ -486,13 +486,15 @@ abstract class Zend_XmlRpc_Value
      */
     protected static function _extractTypeAndValue(SimpleXMLElement $xml, &$type, &$value)
     {
-        list($type, $value) = each($xml);
+        list($type, $value) = current($xml);
+        next($xml);
 
         if (!$type and $value === null) {
             $namespaces = array('ex' => 'http://ws.apache.org/xmlrpc/namespaces/extensions');
             foreach ($namespaces as $namespaceName => $namespaceUri) {
                 $namespaceXml = $xml->children($namespaceUri);
-                list($type, $value) = each($namespaceXml);
+                list($type, $value) = current($namespaceXml);
+                next($namespaceXml);
                 if ($type !== null) {
                     $type = $namespaceName . ':' . $type;
                     break;
